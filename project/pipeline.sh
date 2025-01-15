f54007bc44886f0fe7a34777c8e85d74ebfd823b
Syed Hammad Ali <43202991+AliSyedHammad@users.noreply.github.com>
2025-01-15 17:29:03 +0100

    rechecking sh file

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+!/bin/bash
 
 # Path to the notebook
-# NOTEBOOK_PATH="/home/ubuntu/Made-W24-Repo/project/pipeline.ipynb"
+NOTEBOOK_PATH="/home/ubuntu/Made-W24-Repo/project/pipeline.ipynb"
 
 # Convert the notebook to a Python script
-# jupyter nbconvert --to script "$NOTEBOOK_PATH"
+jupyter nbconvert --to script "$NOTEBOOK_PATH"
 
 python project/pipeline.py
